a575e1ebf92d613dcfc8723b1c99493c544f7b68
Lisa-Baumgaertner <46650897+Lisa-Baumgaertner@users.noreply.github.com>
2025-01-30 15:57:29 +0100

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@ import user from '../fixtures/login.json'
 
 describe('Login', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3008/');
+    cy.visit('http://localhost:3000/');
 
   })
   it('Checks if all fields are present', () => {
